e6fa7a643148fcd6db93fed842076ba599a1a2f7
Gal Bence <bence.gal@onespire.hu>
2020-05-07 15:52:44 +0200

    Adding JSON Model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,21 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], function (Controller) {
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast",
+  "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
   "use strict";
   return Controller.extend("one_walkthrough.controller.App", {
+     onInit : function () {
+        var oData = {
+           recipient : {
+              name : "World"
+           }
+        };
+        var oModel = new JSONModel(oData);
+        this.getView().setModel(oModel);
+     },
      onShowHello : function () {
-        alert("Hello World");
+        MessageToast.show("Hello World");
      }
   });
 });
