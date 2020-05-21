02bf222db45a7811d75254f6e372874c1f0ad896
Gal Bence <bence.gal@onespire.hu>
2020-05-21 16:26:28 +0200

    Adding manifest.json

--- webapp/Component.js
+++ webapp/Component.js
@@ -5,31 +5,25 @@ sap.ui.define([
 ], function (UIComponent, JSONModel, ResourceModel) {
   "use strict";
   return UIComponent.extend("one_walkthrough.Component", {
-     metadata : {
-        rootView: {
-           "viewName": "one_walkthrough.view.App",
-           "type": "XML",
-           "async": true,
-           "id": "app"
-        }
-     },
-     init : function () {
-        // call the init function of the parent
-        UIComponent.prototype.init.apply(this, arguments);
-        // set data model
-        var oData = {
-           recipient : {
-              name : "World"
-           }
-        };
-        var oModel = new JSONModel(oData);
-        this.setModel(oModel);
+    metadata: {
+      manifest: "json"
+    },
+    init: function () {
 
-        // set i18n model
-        var i18nModel = new ResourceModel({
-           bundleName : "one_walkthrough.i18n.i18n"
-        });
-        this.setModel(i18nModel, "i18n");
-     }
+      UIComponent.prototype.init.apply(this, arguments);
+
+      var oData = {
+        recipient: {
+          name: "World"
+        }
+      };
+      var oModel = new JSONModel(oData);
+      this.setModel(oModel);
+
+      var i18nModel = new ResourceModel({
+        bundleName: "one_walkthrough.i18n.i18n"
+      });
+      this.setModel(i18nModel, "i18n");
+    }
   });
 });
