6656954e67aee6471df3e8e721bba118b5134a7a
Gal Bence <bence.gal@onespire.hu>
2020-06-11 16:40:13 +0200

    Adding custom formatters

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,11 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+  "sap/ui/model/json/JSONModel",
+  "../model/formatter"
+], function (Controller, JSONModel, formatter) {
 	"use strict";
 
 	return Controller.extend("one_walkthrough.controller.InvoiceList", {
 
+    formatter : formatter,
 		onInit : function () {
 			var oViewModel = new JSONModel({
 				currency: "EUR"
